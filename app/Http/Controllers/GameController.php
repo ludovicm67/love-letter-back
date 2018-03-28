31ee70c5619d7eec54f88da02b7a3b8fb6a8b3ad
ludovicm67 <ludovicmuller1@gmail.com>
2018-03-29 01:20:01 +0200

    close #6 : fake api routes for games

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use App\Events\TestEvent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
@@ -21,7 +22,7 @@ class GameController extends Controller
         'hello' => 'world'
       ]);
       event($event);
-      dd("ok");
+      dd('ok');
     }
 
     public function create() {
@@ -30,6 +31,54 @@ class GameController extends Controller
         'data' => [
           'game_id' => Uuid::uuid4()
         ]
+      ]);
+    }
+
+    public function list() {
+      $uuid1 = Uuid::uuid4();
+      $uuid2 = Uuid::uuid4();
+      $uuid3 = Uuid::uuid4();
+      return response()->json([
+        'success' => true,
+        'data' => [
+          "$uuid1" => 'ok',
+          "$uuid2" => 'ok',
+          "$uuid3" => 'ok'
+        ]
+      ]);
+    }
+
+    public function waitlist() {
+      $uuid1 = Uuid::uuid4();
+      $uuid2 = Uuid::uuid4();
+      $uuid3 = Uuid::uuid4();
+      return response()->json([
+        'success' => true,
+        'data' => [
+          "$uuid1" => 'ok',
+          "$uuid2" => 'ok',
+          "$uuid3" => 'ok'
+        ]
+      ]);
+    }
+
+    public function join(Request $request) {
+
+      $params = $request->only('game_id');
+      $rules = [
+          'game_id' => 'required|string|max:255'
+      ];
+      $validator = Validator::make($params, $rules);
+      if ($validator->fails()) {
+        return response()->json([
+          'success' => false,
+          'error' => $validator->messages()
+        ]);
+      }
+
+      return response()->json([
+        'success' => true,
+        'data' => 'ok'
       ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,34 +1,15 @@
 <?php
 
-// use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-
 // all routes accessible by everybody
 Route::post('register', 'AuthController@register');
 Route::post('login', 'AuthController@login');
-// Route::post('recover', 'AuthController@recover');
 
-// all routes where we need to be signed in
+// all routes where we need to be signed in (token needed)
 Route::group(['middleware' => ['jwt.auth']], function() {
     Route::get('logout', 'AuthController@logout');
-    Route::get('test', function() {
-        return response()->json(['hello' => 'world']);
-    });
 
     Route::get('game/create', 'GameController@create');
+    Route::get('game/list', 'GameController@list');
+    Route::get('game/waitlist', 'GameController@waitlist');
+    Route::post('game/join', 'GameController@join');
 });
