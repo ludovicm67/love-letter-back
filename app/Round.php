521c0d680acbb24044cb089198a8694764374f4a
sylvestre <martin-heitz@orange.fr>
2018-04-05 17:34:53 +0200

    add some functions

--- app/Round.php
+++ app/Round.php
@@ -6,20 +6,46 @@ use Illuminate\Database\Eloquent\Model;
 
 class Round extends Model
 {
-    protected pile;
-    protected playedcards;
-    protected inGamePlayers;
-    protected currentPlayer;
-    protected gameId;
+    protected $pile;
+    protected $playedCards;
+    protected $inGamePlayers;
+    protected $currentPlayer;
+    protected $gameId;
+    protected $isFinished;
 
-    public function createRound(players, deck, id) {
+    public function createRound($players, $deck, $id) 
+    {
+      $pile = $deck;
+      $playedCards = array(); //tableau vide
+      $inGamePlayers = $players;
+      $currentPlayer = array_values($players)[0];
+      $gameId = $id;
+      $isFinished = false;
+    }
 
-      pile = desk;
-      playercards = []; //table vide
-      inGamePlayers =players;
-      currentPlayer=players[0];
-      gameId= id;
+    public function setCurrentPlayer($player)
+    {
+    	$currentPlayer = $player;
+    }
 
+    public function getIsFinished()
+    {
+    	return $isFinished;
+    }
 
+    public function setIsFinished()
+    {
+    	$isFinished = true;
+    }
+
+    public function setInGamePlayers($player) //a player has been eliminated
+    {
+    	$number = array_search($player->name, array_column($inGamePlayers, 'name'));
+    	array_splice($inGamePlayers, $number);
+
+    	if(count($inGamePlayers) == 1)
+    	{
+    		setIsFinished();
+    	}
     }
 }
