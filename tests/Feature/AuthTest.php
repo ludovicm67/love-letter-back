65b6b7d283b76d13b23cccc7968d875129bfa556
ludovicm67 <ludovicmuller1@gmail.com>
2018-04-18 19:46:26 +0200

    added login tests

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -8,7 +8,7 @@ use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
-  public function testRegister()
+  public function testRegisterNewUser()
   {
     $user = User::whereName('TESTJohn');
     $user->delete();
@@ -47,5 +47,107 @@ class AuthTest extends TestCase
       property_exists($res->data->user, 'lost_games'),
       'no user.lost_games property'
     );
+  }
+
+  public function testRegisterExistingUser()
+  {
+    $user = User::whereName('TESTJohn');
+    $user->delete();
+
+    // first one: create user
+    $response = $this->json('POST', '/api/register', [
+      'name' => 'TESTJohn',
+      'password' => 'TESTDoe'
+    ]);
+
+    // do the request a second one
+    $response = $this->json('POST', '/api/register', [
+      'name' => 'TESTJohn',
+      'password' => 'TESTDoe'
+    ]);
+
+    $response->assertStatus(400)->assertJson(['success' => false]);
+  }
+
+  public function testLogin()
+  {
+    $user = User::whereName('TESTJohn');
+    $user->delete();
+
+    // create user
+    $response = $this->json('POST', '/api/register', [
+      'name' => 'TESTJohn',
+      'password' => 'TESTDoe'
+    ]);
+
+    // login
+    $response = $this->json('POST', '/api/login', [
+      'name' => 'TESTJohn',
+      'password' => 'TESTDoe'
+    ]);
+
+    $response->assertStatus(200)->assertJson(['success' => true]);
+    $res = json_decode($response->content());
+    $this->assertTrue(property_exists($res, 'data'), 'no data property');
+    $this->assertTrue(
+      property_exists($res->data, 'token'),
+      'no token property'
+    );
+    $this->assertNotEmpty($res->data->token);
+    $this->assertTrue(property_exists($res->data, 'user'), 'no user property');
+    $this->assertTrue(
+      property_exists($res->data->user, 'id'),
+      'no user.id property'
+    );
+    $this->assertTrue(
+      property_exists($res->data->user, 'name'),
+      'no user.name property'
+    );
+    $this->assertTrue(
+      property_exists($res->data->user, 'points'),
+      'no user.points property'
+    );
+    $this->assertTrue(
+      property_exists($res->data->user, 'won_games'),
+      'no user.won_games property'
+    );
+    $this->assertTrue(
+      property_exists($res->data->user, 'lost_games'),
+      'no user.lost_games property'
+    );
+  }
+
+  public function testLoginBadCredentials()
+  {
+    $user = User::whereName('TESTJohn');
+    $user->delete();
+
+    // create user
+    $response = $this->json('POST', '/api/register', [
+      'name' => 'TESTJohn',
+      'password' => 'TESTDoe'
+    ]);
+
+    // login
+    $response = $this->json('POST', '/api/login', [
+      'name' => 'TESTJohn',
+      'password' => 'TESTJohn'
+    ]);
+
+    $response->assertStatus(401)->assertJson(['success' => false]);
+  }
+
+  public function testLoginNonExistingUser()
+  {
+    $user = User::whereName('TESTJohn');
+    $user->delete();
+
+    // try to login
+    $response = $this->json('POST', '/api/login', [
+      'name' => 'TESTJohn',
+      'password' => 'TESTDoe'
+    ]);
+
+    $response->assertStatus(401)->assertJson(['success' => false]);
   }
 }
